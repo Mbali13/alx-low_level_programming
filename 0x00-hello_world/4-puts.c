76ad93fe5047e7dbc92440967714677a175f9df5
Mbelenhle Mabena <mabenaenhlee@gmail.com>
2023-10-05 12:16:40 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+
+/""
+ " main - this is the main function
+ "
+ " Return: always 0
+ "/
+
 int main(void)
 (
         puts("\"Programming is like building a multilingual puzzle):
