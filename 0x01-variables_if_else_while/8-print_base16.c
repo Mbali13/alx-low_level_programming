4a025fd55efe16db8c40a0bb4f2b272816d21a38
Mbelenhle Mabena <mabenaenhlee@gmail.com>
2023-10-07 06:50:13 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main funtion
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	char g;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (g = 'a' ; g <= 'f' ; g++)
+		putchar(g);
+	putchar('\n');
+	return (0);
+}
