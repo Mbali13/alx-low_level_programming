d1338440dc002f90e96b5372a33978342b7a2fb2
Mbelenhle Mabena <mabenaenhlee@gmail.com>
2023-10-11 13:42:17 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: number to start from
+ * Return: void
+ */
 
 void print_to_98(int n)
 {
@@ -9,7 +14,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -17,17 +24,22 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
-			printf("\n");
+		printf("\n");
 	}
 	else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
+	else if (n == 98)
 		printf("%d\n", n);
 }
