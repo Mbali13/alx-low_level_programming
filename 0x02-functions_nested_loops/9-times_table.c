69ac63c400843192e5f63bfe555907c436288ea5
Mbelenhle Mabena <mabenaenhlee@gmail.com>
2023-10-11 07:41:12 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+void times_table(void)
+{
+	int i, g, result;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (g = 0 ; g < 10 ; g++)
+		{
+			result = i * g;
+			if (g == 0)
+				printf("%d, ", result);
+			else
+			{
+				printf("%2d", result);
+				if (g != 9)
+					printf(", ");
+			}
+		}
+		printf("\n");
+	}
+}
