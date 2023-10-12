9e63f75ede370e98d1dc7c45cdb55a37932cbd25
Mbelenhle Mabena <mabenaenhlee@gmail.com>
2023-10-12 06:07:30 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 numbers
+ * @a: function parameter a
+ * @b: function parameter b
+ * Return: void
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
